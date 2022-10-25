515edb4f834ae0524dcc1d06d4d19ab008c8540f
rchrdpdrz <rchrd.pdrz.mrtnz@gmail.com>
2022-10-25 19:27:50 -0400

    hola

--- src/app.py
+++ src/app.py
@@ -4,6 +4,10 @@ from config import config
 
 app=Flask(__name__)
 
+@app.route('/')
+def init():
+    return render_template('layout.html')
+
 if __name__ == '__main__':
     app.config.from_object(config['development'])
     app.run()
