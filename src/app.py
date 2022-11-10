22304ddb6699dc6054ed7cc1a8bf430eaf3cc51f
rchrdpdrz <rchrd.pdrz.mrtnz@gmail.com>
2022-11-09 22:11:22 -0400

    log user

--- src/app.py
+++ src/app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_mysqldb import MySQL
+from flask_login import LoginManager, login_user, logout_user, login_required
 from config import config
 #models
 from models.ModelUser import ModelUser
 #entities
 from models.entities.User import User
 
-
 app=Flask(__name__)
+
 db=MySQL(app)
+
+login_manager_app=LoginManager(app)
+
+@login_manager_app.user_loader(id)
+def load_user(id):
+    return ModelUser.get_by_id(db, id)
 
 @app.route('/')
 def home():
@@ -24,16 +31,18 @@ def login():
         logged_user=ModelUser.login(db,user)
         if logged_user != None:
             if logged_user.password:
-                return redirect(url_for('home'))
+                logged_user(login_user)
+                return redirect(url_for('system'))
             else:
                 return render_template('auth/login.html',data=data)
         else:
             return render_template('auth/login.html',data=data)
     else:
         return render_template('auth/login.html',data=False)
-    
 
-
+@app.route('/app')
+def system():
+    return render_template('system.html')
 
 def page_not_found(error):
     return render_template('404.html'), 404
--- src/models/ModelUser.py
+++ src/models/ModelUser.py
@@ -16,3 +16,18 @@ class ModelUser():
                 return None
         except Exception as ex:
             raise Exception(ex)
+
+    @classmethod
+    def get_by_id(self, db, id):
+        try:
+            cursor = db.connection.cursor()
+            sql = f"""SELECT id, username, fullname FROM users 
+                    WHERE id = '{id}'"""
+            cursor.execute(sql)
+            row = cursor.fetchone()
+            if row != None:
+                return User(row[0], row[1], None, row[2])
+            else:
+                return None
+        except Exception as ex:
+            raise Exception(ex)
